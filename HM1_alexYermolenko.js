09297b3d11862ae9827e6077e71a3277c71ba3c1
ven0m79 <yermolenko79@gmail.com>
2022-02-17 12:44:11 +0200

    HW1_final_unsorted

--- HM1_alexYermolenko.js
+++ HM1_alexYermolenko.js
@@ -9,12 +9,36 @@ let summLow = Math.floor(kodein) + Math.floor(morphin) + Math.floor(met);
 let summHundred = Math.round(summLow/100)*100;
 let keepChange = 500 - summ;
 
+
+
+if (Math.floor(summ)%2 == 0) {
+    console.log('Сума товарів є парним числом так як вона дорівнює - ' + Math.floor(summ))
+} else console.log('Сума товарів є непарним числом так як вона дорівнює - ' + Math.floor(summ))
+
+
+let odd;
+odd = Math.floor(summ)%2 == 0;
+console.log('Сума парна? - ' + odd);
+
+ 
 console.log('Решту з 500 отримає ' + keepChange);
 
-if (Math.floor(summ)/2 == 0) {
-    console.log('Сума товарів є парним числом ' + true + ' так як вона дрівнює - ' + Math.floor(summ))
-} else console.log('Сума товарів є непарним числом ' + false + ' так як вона дрівнює - ' + Math.floor(summ))
- 
+let middlePrice;
+middlePrice = (kodein + morphin + met)/3;
+console.log('Середнє значення цін, округлене до другого знаку після коми - ' + middlePrice.toFixed(2));
+
+
+let discount;
+let halfPrice;
+discount = Math.random()*(50);
+console.log('Випадкова знижка - ' + discount);
+console.log('Ваша знижка ' + discount + '. ' + 'Сума до сплати - ' + (summ - discount).toFixed(2));
+
+halfPrice = summ/2;
+profit = halfPrice - discount;
+console.log('Пів ціни - ' + halfPrice);
+console.log('Чистий прибуток складає - ' + profit);
+
 
 console.log('Сума товарів округлена до сотен = ' + summHundred);
 console.log('Сума товарів без копійок = ' + summLow);
