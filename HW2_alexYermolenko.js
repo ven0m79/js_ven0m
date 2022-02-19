caefe15f08e38846f1f670c34457ebc87539ade6
ven0m79 <yermolenko79@gmail.com>
2022-02-19 20:56:03 +0200

    HW2 on-the-middle

--- HW2_alexYermolenko.js
+++ HW2_alexYermolenko.js
@@ -1,6 +1,21 @@
 let n, m;
-let a, b;
-a = prompt(n);
-b = prompt(m);
-console.log(a);
-console.log(b);
+let sum;
+
+do {
+ n = +(prompt('Введіть ціле число N', 10));
+}
+while (!Number.isInteger(n));
+console.log(n);
+
+do {
+    m = +(prompt('Введіть ціле число M (повинно бути більше N)', 20));
+   }
+   while (!Number.isInteger(m) || m <= n);
+console.log(m);
+
+if (confirm('Пропускати парні числа?')) {
+sum = n +
+
+
+};
+//n % 2 == 0;
