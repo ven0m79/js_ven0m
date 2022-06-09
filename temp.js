cf160d57a215989e98c4713e015578835b4e7377
ven0m <yermolenko79@gmail.com>
2022-06-09 20:49:15 +0300

    HW5-7d_func

--- temp.js
+++ temp.js
@@ -1,9 +1,23 @@
-//7. Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті, які діляться на ціло на 5
-//Приклад: getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) -> [55, 55]
+//Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*). При вирішенні цього завдання необхідно розбити масив на слова за допомогою
+// функції .split(" "), після чого масив необхідно буде склеїти .join(" ") Погані слова: shit та fuck. Передбачте можливість розширювати список цих слів у майбутньому.
+//Приклад: replaceBadWords("Are you fucking kidding?") -> "Are you ****ing kidding?" Приклад: replaceBadWords("Holy shit!") -> "Holy ****!" 
+//Приклад: replaceBadWords("It's bullshit!") -> "It's bull****!"
 
-const getDividedByFive = function(...numbers) {
-return numbers.filter((item) => {
-  return item % 5 === 0
-})
+const replaceBadWords = (string) => {
+const badWordsArray = ['fuck', 'shit'];
+newArray = string.split(' ')
+for (let i = 0; i < newArray.length; i++){
+  if (newArray[i])
+  console.log(newArray[i])
+
 }
-console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2))
+
+
+
+let temp = newArray.map((item) => {
+  console.log(badWordsArray[1])
+  item === badWordsArray[0]
+}) 
+return temp
+}
+console.log(replaceBadWords('Are you fucking kidding?'))
