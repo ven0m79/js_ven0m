d71003c9c48cbede638d3aa6c7211f4186ea4b31
ven0m <yermolenko79@gmail.com>
2022-06-12 10:50:49 +0300

    HW7-start_func

--- temp.js
+++ temp.js
@@ -1,42 +1,11 @@
-const students = [{
-  name: "Tanya",
-  course: 3,
-  subjects: {
-    math: [4, 4, 3, 4],
-    algorithms: [3, 3, 3, 4, 4, 4],
-    data_science: [5, 5, 3, 4]
-  }
-}, {
-  name: "Victor",
-  course: 4,
-  subjects: {
-    physics: [5, 5, 5, 3],
-    economics: [2, 3, 3, 3, 3, 5],
-    geometry: [5, 5, 2, 3, 5]
-  }
-}, {
-  name: "Anton",
-  course: 2,
-  subjects: {
-    statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
-    english: [5, 3],
-    cosmology: [5, 5, 5, 5]
-  }
-}];
+//Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*). 
+//При вирішенні цього завдання необхідно розбити масив на слова за допомогою функції .split(" "), після чого масив необхідно буде склеїти .join(" ") 
+//Погані слова: shit та fuck. Передбачте можливість розширювати список цих слів у майбутньому.
+//Приклад: replaceBadWords("Are you fucking kidding?") -> "Are you ****ing kidding?" Приклад: replaceBadWords("Holy shit!") -> "Holy ****!" 
+//Приклад: replaceBadWords("It's bullshit!") -> "It's bull****!"
 
+const replaceBadWords = ((string) => {
+const badWords = 'shit, fuck'
 
-//2. Створіть функцію getAverageMark(students[0]) --> 3.79 – яка поверне середню оцінку по усім предметам для переданого студента НЕ МАСИВА СТУДЕНТІВ. 
-//Оцінку округліть до 2ого знаку. Можна використовувати функції, написані у попередніх домашніх завданнях :)
-
-const getAverageMark = ((stud) => {
-  let newArray = [];
-  const arrayMarks = Object.values(stud.subjects)
-  for (let i = 0; i < arrayMarks.length; i++) {
-    newArray = newArray.concat(arrayMarks[i])
-    }
-  const result = newArray.reduce((acc, item) => {
-  return acc  +  item 
-  })
-  return (result / newArray.length).toFixed(2)
-  })
-  console.log(getAverageMark(students[0]))
+})
+console.log(replaceBadWords)
