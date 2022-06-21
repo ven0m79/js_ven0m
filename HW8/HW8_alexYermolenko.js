053f37dcbd01bb2fa2fc61e0beace1d74e1396b5
ven0m <yermolenko79@gmail.com>
2022-06-21 13:33:16 +0300

    HW8-base+adv

--- HW8/HW8_alexYermolenko.js
+++ HW8/HW8_alexYermolenko.js
@@ -57,3 +57,38 @@ console.log(user.marksOfStudent);
 user.recover();
 console.log(user.flag)
 console.log(user.marksOfStudent);
+
+//ADVANCED
+
+class BudgetStudent extends Students {
+  constructor(scholarShip, university, course, fullName, marks, flag) {
+    super(university, course, fullName, marks, flag);
+    this.scholarShip = scholarShip;
+    let timerId = setInterval (function() {
+      this.getScholarship()
+
+    }.bind(this), 3000);
+    setTimeout(() => {
+      clearInterval(timerId)}, 30000)   
+    
+      
+  }
+
+   getScholarship = (() => {
+    if(this.flag == false || this.getAverageMark() < 4) {
+    this.scholarShip = null;
+    console.log(this.fullName + ' Дульки тобі а не степуху)') 
+    return}
+    else { 
+      this.scholarShip += 1400;
+      console.log(this.fullName + ` Тримай степуху! - 1400 вже відправлені тобі по е-мейлу :D. Вже назбиралось ` + this.scholarShip + ` гривасєй`) 
+    return
+    }
+  })
+}
+const studentAdv1 = new BudgetStudent(0, 'KPI', 2, 'Bogdan Muranets', [4, 5, 3, 5], true);
+const studentAdv2 = new BudgetStudent(0, 'KPI', 1, 'Andriy Dovbenko', [4, 5, 2, 3], false);
+const studentAdv3 = new BudgetStudent(0, 'KPI', 4, 'Sergiy Murzak', [4, 2, 2, 3], true);
+
+
+
