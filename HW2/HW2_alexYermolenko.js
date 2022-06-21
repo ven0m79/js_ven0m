5938d9fe0b6d80c38829e9d06ba0a04828b61faa
ven0m <yermolenko79@gmail.com>
2022-06-21 14:52:29 +0300

    HW9-start

---
+++ HW2/HW2_alexYermolenko.js
@@ -0,0 +1,39 @@
+let n, m, even;
+let sum = 0;
+
+do {
+ n = +prompt('Введіть ціле число N', 10);
+}
+while (!Number.isInteger(n) || !n);
+console.log(n);
+
+do {
+    m = +prompt('Введіть ціле число M (повинно бути більше N)', 100);
+}
+   while (!Number.isInteger(m) || m <= n || !n);
+console.log(m);
+
+if (even = confirm('Пропускати парні числа?')) {
+    for (let i = n; i < m; i++) {
+        if (i % 2 == 0){
+            i++;
+            sum = sum + i;
+            console.log('i = ' + i);
+            console.log('Сумма чисед від N до М дорівнює: ' + sum)
+        } else  sum = i;
+    }
+} else
+    for (let i = n; i <= m; i++) {
+        console.log('i = ' + i);
+        sum = sum + i;
+        console.log('Сумма чисед від N до М дорівнює: ' + sum)
+    }
+console.log('Сума чисел буде дорівнювати - ' + sum);
+
+document.writeln(
+    `<p>Ви ввели N: ${n}</br>
+     Ви ввели M: ${m}</p>
+     <p>Пропускати парні: ${even}</p>
+     <p>Сумма чисел від N до М дорівнює: ${sum}</p>
+     <div class=a>* підрахунки можна глянути в консолі</div>`)
+     ;
