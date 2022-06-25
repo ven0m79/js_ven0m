7a310fb132242159776242f080fe4a128ffd0c08
ven0m <yermolenko79@gmail.com>
2022-06-25 13:05:09 +0300

    HW10-on-middle1

--- HW10/HW10_alexYermolenko.js
+++ HW10/HW10_alexYermolenko.js
@@ -1,9 +1,14 @@
 
 const audioArray = [
+    new Audio ('sound/ak47.mp3'),
+    new Audio ('sound/aug.mp3'),
     new Audio ('sound/awp.mp3'),
+    new Audio ('sound/deagle.mp3'),
+    new Audio ('sound/mp5.mp3'),
+    new Audio ('sound/usp.mp3'),
 
 ]
-const audioObj = new Audio('sound/mp5.mp3');
+const audioObj = new Audio('sound/usp.mp3');
 
 const circ = document.querySelector('.circ-button');
 
