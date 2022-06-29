ecec9d03846372b865f48e489ae71ff0bd68ab6f
ven0m79 <yermolenko79@gmail.com>
2022-06-29 16:08:20 +0300

    HW10-final1

--- HW10/HW10_alexYermolenko.js
+++ HW10/HW10_alexYermolenko.js
@@ -1,30 +1,31 @@
-
+const longList = document.querySelector("#wrapper");
 const audioArray = [
-    new Audio ('sound/ak47.mp3'),
-    new Audio ('sound/aug.mp3'),
-    new Audio ('sound/awp.mp3'),
-    new Audio ('sound/deagle.mp3'),
-    new Audio ('sound/mp5.mp3'),
-    new Audio ('sound/usp.mp3'),
-
+    new Audio('sound/ak47.mp3'),
+    new Audio('sound/aug.mp3'),
+    new Audio('sound/awp.mp3'),
+    new Audio('sound/deagle.mp3'),
+    new Audio('sound/mp5.mp3'),
+    new Audio('sound/usp.mp3'),
 ]
-/* const audioObj = new Audio('sound/usp.mp3');
+const keyArray = ['KeyA', 'KeyS', 'KeyD', 'KeyJ', 'KeyK', 'KeyL']
 
-const circ = document.querySelector('.circ-button');
-
-circ.addEventListener("click", event => {
-
-    audioObj.play();
-  });
- */
-
-
-  function doSmth(target) {
-    console.log(target.id);
-        audioArray[target.id - 1]?.currentTime = 0;
-        audioArray[target.id - 1]?.play();
-
+function playSmth(e) {
+    if (e.target.id !== "wrapper") {
+        console.log(e.target.id);
+        audioArray[e.target.id - 1].currentTime = 0;
+        audioArray[e.target.id - 1].play();
     }
-   
-   const longList = document.querySelector("#wrapper");
-   wrapper.addEventListener("click", (e) =>  doSmth(e.target))
+}
+
+longList.addEventListener("click", playSmth);
+window.addEventListener("keydown", (e) => {
+    keyArray.forEach ((item, index) => {
+        if (item === e.code) {
+            console.log(audioArray[index]);
+            audioArray[index].currentTime = 0;
+            audioArray[index].play();
+        }
+
+    })
+})
+
