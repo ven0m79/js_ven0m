0ad2eb00a2ffbf61f3b949166305a0f3eed3d1d0
ven0m79 <yermolenko79@gmail.com>
2022-06-29 13:23:51 +0300

    test10

--- HW10/HW10_alexYermolenko.js
+++ HW10/HW10_alexYermolenko.js
@@ -8,11 +8,23 @@ const audioArray = [
     new Audio ('sound/usp.mp3'),
 
 ]
-const audioObj = new Audio('sound/usp.mp3');
+/* const audioObj = new Audio('sound/usp.mp3');
 
 const circ = document.querySelector('.circ-button');
 
 circ.addEventListener("click", event => {
-    /* аудио может быть воспроизведено; проиграть, если позволяют разрешения */
+
     audioObj.play();
   });
+ */
+
+
+  function doSmth(target) {
+    console.log(target.id);
+        audioArray[target.id - 1]?.currentTime = 0;
+        audioArray[target.id - 1]?.play();
+
+    }
+   
+   const longList = document.querySelector("#wrapper");
+   wrapper.addEventListener("click", (e) =>  doSmth(e.target))
