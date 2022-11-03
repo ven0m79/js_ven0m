66462b0b67c018b4dfbb8920d8267e53765d32f3
ven0m79 <yermolenko79@gmail.com>
2022-11-03 17:58:28 +0200

    CW15-fina15

--- my-app-hookes/src/App.js
+++ my-app-hookes/src/App.js
@@ -1,26 +1,45 @@
 //import logo from './logo.svg';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
-function App() {
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
-  const firstNameChange = (e) => {
-    setFirstName(e.target.value);
-  }
-  const lastNameChange = (e) => {
-    setLastName(e.target.value);
-  }
+function App() { //stateless. In class - statefull. 
+  const [users, setUsers] = useState(null);
+  const [value, setValue] = useState("");
+
+  const needUpdate = value === "aaa";
+  const calculate = useMemo(() => {
+    if (needUpdate) {let value = 0;
+      for (let i = 0; i <= 100000; i++) {
+        value += i;
+        
+      }
+      console.log({value});
+      return value
+      
+    }
+    
+  },[needUpdate])
   
-  const submit = () => {
-    console.log({firstName, lastName})
-  }
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((res) => res.json())
+  .then(setUsers);
+}, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <input placeholder='First name' onChange={firstNameChange} />
-        <input placeholder='Last name' onChange={lastNameChange} />
-        <button onClick={submit}>Click ME!</button>
+        {/*<button onClick={handleUp}>Up!</button>
+        <button onClick={handleDown}>Down!</button>
+        <h2>Counter: {counter}</h2>*/}
+
+      <input value={value} onChange={(e) => setValue(e.target.value)}/>
+        <h2>{calculate}</h2>
+
+        {users ? users.map((el) => {
+          return <div key={el.id}>{el.name}</div>
+        })
+        : null}
       </header>
       
     </div>
